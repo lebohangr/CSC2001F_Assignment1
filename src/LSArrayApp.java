a4e01ea3fb88d1ab9cbcea1306248138a9c74c2b
Lebo Ramachela <RMCLEB001@myuct.ac.za>
2020-03-01 15:06:42 +0200

    First commit

---
+++ src/LSArrayApp.java
@@ -0,0 +1,9 @@
+public class LSArrayApp {
+    public static void main(String[] args) {
+        String[] data = new String[12];
+        data[0] = "Connor";
+        data[1] = "John";
+        data[2] = "Mary";
+        System.out.printf("%s %s %s",data[0],data[1], data[2]);
+    }
+}
